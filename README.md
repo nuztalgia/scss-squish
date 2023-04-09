c744f4387b0975be3f66b4bf56d456c688959d19
nuztalgia <nuztalgia@gmail.com>
2023-04-09 13:31:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🤏 scss-squish
+
+A niche little tool for squishing SCSS files together.
+
+More info coming soon. Stay tuned!
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    install_requires=[
+        "pyperclip ==1.8.2",
+        "requests ==2.28.2",
+    ],
+    url="https://github.com/nuztalgia/scss-squish",
+)
